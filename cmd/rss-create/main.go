3805d4a67853ce090140994b7a9f93297fd04254
codesoap <codesoap@mailbox.org>
2019-07-29 10:46:26 +0200

    Add rss-create

---
+++ cmd/rss-create/main.go
@@ -0,0 +1,313 @@
+package main
+
+import (
+	"encoding/xml"
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/codesoap/rss2"
+	"github.com/docopt/docopt-go"
+)
+
+var usage = `
+Create an RSS 2.0 file. The file will contain no RSS items.
+
+Usage:
+    rss-create --title=<title>
+               --link=<link>
+               --description=<desc>
+               [--language=<lang>]
+               [--copyright=<notice>]
+               [--managingEditor=<name>]
+               [--webMaster=<name>]
+               [--pubDate=<date>]
+               [--lastBuildDate=<date>]
+               [(  --category=<category>
+                   [--category-domain=<domain>]
+               )]
+               [--generator=<generator>]
+               [--docs=<docs>]
+               [(  --cloud-domain=<domain>
+                   --cloud-port=<port>
+                   --cloud-path=<path>
+                   --cloud-registerProcedure=<rp>
+                   --cloud-protocol=<protocol>
+               )]
+               [--ttl=<ttl>]
+               [(  --image-url=<url>
+                   --image-title=<title>
+                   --image-link=<link>
+                   [--image-width=<width>]
+                   [--image-height=<height>]
+                   [--image-description=<desc>]
+               )]
+               [--rating=<rating>]
+               [(  --textInput-title=<title>
+                   --textInput-description=<desc>
+                   --textInput-name=<name>
+                   --textInput-link=<link>
+               )]
+               [(--skipHour=<hour>)...]
+               [(--skipDay=<day>)...]
+               [<outfile>]
+`
+
+type conf struct {
+	Title                  string
+	Link                   string
+	Description            string
+	Language               string
+	Copyright              string
+	ManagingEditor         string
+	WebMaster              string
+	PubDate                string
+	LastBuildDate          string
+	Category               string
+	CategoryDomain         string
+	Generator              string
+	Docs                   string
+	CloudDomain            string
+	CloudPort              int
+	CloudPath              string
+	CloudRegisterProcedure string
+	CloudProtocol          string
+	Ttl                    int
+	ImageUrl               string
+	ImageTitle             string
+	ImageLink              string
+	ImageWidth             int
+	ImageHeight            int
+	ImageDescription       string
+	Rating                 string
+	TextInputTitle         string
+	TextInputDescription   string
+	TextInputName          string
+	TextInputLink          string
+	// SkipHour should be []int, but docopt-go doesn't support it.
+	SkipHour []string
+	SkipDay  []string
+	Outfile  string
+}
+
+func main() {
+	opts, err := docopt.ParseDoc(usage)
+	if err != nil {
+		// No need to print anything here, since ParseDoc() already does.
+		os.Exit(1)
+	}
+	var conf conf
+	err = opts.Bind(&conf)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, `Error when using arguments:`, err.Error())
+		os.Exit(2)
+	}
+	channel, err := rss2.NewChannel(conf.Title, conf.Link, conf.Description)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, `Error when creating RSS channel:`, err.Error())
+		os.Exit(3)
+	}
+	err = addOptionalElements(channel, &conf)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, `Error when parsing arguments:`, err.Error())
+		os.Exit(3)
+	}
+	rss := rss2.NewRSS(channel)
+	if err = printOrWriteResult(rss, &conf); err != nil {
+		fmt.Fprintln(os.Stderr, `Error when rendering:`, err.Error())
+		os.Exit(4)
+	}
+}
+
+func addOptionalElements(channel *rss2.Channel, conf *conf) (err error) {
+	if len(conf.Language) > 0 {
+		channel.Language = conf.Language
+	}
+	if len(conf.Copyright) > 0 {
+		channel.Copyright = conf.Copyright
+	}
+	if len(conf.ManagingEditor) > 0 {
+		channel.ManagingEditor = conf.ManagingEditor
+	}
+	if len(conf.WebMaster) > 0 {
+		channel.WebMaster = conf.WebMaster
+	}
+	if err = addPubDate(channel, conf); err != nil {
+		return
+	}
+	if err = addLastBuildDate(channel, conf); err != nil {
+		return
+	}
+	if err = addCategory(channel, conf); err != nil {
+		return
+	}
+	if len(conf.Generator) > 0 {
+		channel.Generator = conf.Generator
+	}
+	if len(conf.Docs) > 0 {
+		channel.Docs = conf.Docs
+	}
+	if err = addCloud(channel, conf); err != nil {
+		return
+	}
+	if conf.Ttl > 0 {
+		channel.TTL = conf.Ttl
+	}
+	if err = addImage(channel, conf); err != nil {
+		return
+	}
+	if len(conf.Rating) > 0 {
+		channel.Rating = conf.Rating
+	}
+	if err = addTextInput(channel, conf); err != nil {
+		return
+	}
+	if err = addSkipHours(channel, conf); err != nil {
+		return
+	}
+	if err = addSkipDays(channel, conf); err != nil {
+		return
+	}
+	return
+}
+
+func addPubDate(channel *rss2.Channel, conf *conf) (err error) {
+	if len(conf.PubDate) > 0 {
+		var date rss2.RSSTime
+		date, err = rss2.ParseRSSTime(conf.PubDate)
+		if err != nil {
+			return
+		}
+		channel.PubDate = &date
+	}
+	return
+}
+
+func addLastBuildDate(channel *rss2.Channel, conf *conf) (err error) {
+	if len(conf.LastBuildDate) > 0 {
+		var date rss2.RSSTime
+		date, err = rss2.ParseRSSTime(conf.LastBuildDate)
+		if err != nil {
+			return
+		}
+		channel.LastBuildDate = &date
+	}
+	return
+}
+
+func addCategory(channel *rss2.Channel, conf *conf) (err error) {
+	if len(conf.Category) > 0 {
+		if channel.Category, err = rss2.NewCategory(conf.Category); err != nil {
+			return
+		}
+		if len(conf.CategoryDomain) > 0 {
+			channel.Category.Domain = conf.CategoryDomain
+		}
+	}
+	return
+}
+
+func addCloud(channel *rss2.Channel, conf *conf) (err error) {
+	if len(conf.CloudDomain) > 0 {
+		channel.Cloud, err = rss2.NewCloud(conf.CloudDomain, conf.CloudPort,
+			conf.CloudPath, conf.CloudRegisterProcedure, conf.CloudProtocol)
+	}
+	return
+}
+
+func addTextInput(channel *rss2.Channel, conf *conf) (err error) {
+	if len(conf.TextInputTitle) > 0 {
+		channel.TextInput, err = rss2.NewTextInput(conf.TextInputTitle,
+			conf.TextInputDescription, conf.TextInputName, conf.TextInputLink)
+	}
+	return
+}
+
+func addImage(channel *rss2.Channel, conf *conf) (err error) {
+	if len(conf.ImageUrl) > 0 {
+		channel.Image, err = rss2.NewImage(conf.ImageUrl, conf.ImageTitle,
+			conf.ImageLink)
+		if err != nil {
+			return
+		}
+		if conf.ImageWidth > 0 {
+			channel.Image.Width = conf.ImageWidth
+		}
+		if conf.ImageHeight > 0 {
+			channel.Image.Height = conf.ImageHeight
+		}
+		if len(conf.ImageDescription) > 0 {
+			channel.Image.Description = conf.ImageDescription
+		}
+	}
+	return
+}
+
+func addSkipHours(channel *rss2.Channel, conf *conf) (err error) {
+	if len(conf.SkipHour) > 0 {
+		var hours []int
+		for _, hour := range conf.SkipHour {
+			var hour_int int
+			hour_int, err = strconv.Atoi(hour)
+			if err != nil {
+				return
+			}
+			hours = append(hours, hour_int)
+		}
+		var skipHours *rss2.SkipHours
+		if skipHours, err = rss2.NewSkipHours(hours); err != nil {
+			return
+		}
+		channel.SkipHours = skipHours
+	}
+	return
+}
+
+func addSkipDays(channel *rss2.Channel, conf *conf) (err error) {
+	if len(conf.SkipDay) > 0 {
+		var days []time.Weekday
+		daysLookup := map[string]time.Weekday{
+			`Sunday`:    time.Sunday,
+			`Monday`:    time.Monday,
+			`Tuesday`:   time.Tuesday,
+			`Wednesday`: time.Wednesday,
+			`Thursday`:  time.Thursday,
+			`Friday`:    time.Friday,
+			`Saturday`:  time.Saturday,
+		}
+		for _, day := range conf.SkipDay {
+			var dayWeekday time.Weekday
+			dayWeekday, ok := daysLookup[day]
+			if !ok {
+				return fmt.Errorf(`invalid weekday: %s`, day)
+			}
+			days = append(days, dayWeekday)
+		}
+		var skipDays *rss2.SkipDays
+		skipDays = rss2.NewSkipDays(days)
+		channel.SkipDays = skipDays
+	}
+	return
+}
+
+func printOrWriteResult(rss *rss2.RSS, conf *conf) (err error) {
+	rss_bytes, err := xml.MarshalIndent(rss, ``, "\t")
+	if err != nil {
+		return fmt.Errorf("error when rendering rss: %s", err.Error())
+	}
+	if len(conf.Outfile) == 0 {
+		fmt.Print(xml.Header)
+		fmt.Println(string(rss_bytes))
+	} else {
+		var outfile *os.File
+		outfile, err = os.Create(conf.Outfile)
+		if err != nil {
+			return
+		}
+		defer outfile.Close()
+		fmt.Fprint(outfile, xml.Header)
+		fmt.Fprintln(outfile, string(rss_bytes))
+	}
+	return
+}
